ca7ed452b634301f5aa7e6fdc5e9f9e9606b0b50
davidebbo <david.ebbo@microsoft.com>
2011-10-19 17:10:05 -0700

    Cleanup & refactoring

--- RoslynRazorViewEngine/RoslynRazorViewEngine.cs
+++ RoslynRazorViewEngine/RoslynRazorViewEngine.cs
@@ -1,91 +1,70 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Compilation;
+using System.Web.Hosting;
 using System.Web.Mvc;
+using System.Web.Razor;
+using System.Web.Razor.Parser.SyntaxTree;
 using System.Web.WebPages;
 using System.Web.WebPages.Razor;
-using System.Web.Razor;
-using System.Web.Hosting;
-using System.IO;
 using Microsoft.CSharp;
-using System.CodeDom.Compiler;
-using Roslyn.Compilers.CSharp;
 using Roslyn.Compilers;
-using System.Globalization;
-using System.Web;
-using System.Web.Compilation;
+using Roslyn.Compilers.CSharp;
 
 namespace RoslynRazorViewEngine {
     public class RoslynRazorViewEngine : VirtualPathProviderViewEngine, IVirtualPathFactory {
         public RoslynRazorViewEngine() {
             base.AreaViewLocationFormats = new[] {
                 "~/Areas/{2}/Views/{1}/{0}.cshtml", 
-                "~/Areas/{2}/Views/{1}/{0}.vbhtml", 
-                "~/Areas/{2}/Views/Shared/{0}.cshtml", 
-                "~/Areas/{2}/Views/Shared/{0}.vbhtml"
+                "~/Areas/{2}/Views/Shared/{0}.cshtml"
             };
 
             base.AreaMasterLocationFormats = new[] {
                 "~/Areas/{2}/Views/{1}/{0}.cshtml", 
-                "~/Areas/{2}/Views/{1}/{0}.vbhtml", 
-                "~/Areas/{2}/Views/Shared/{0}.cshtml", 
-                "~/Areas/{2}/Views/Shared/{0}.vbhtml"
+                "~/Areas/{2}/Views/Shared/{0}.cshtml"
             };
 
             base.AreaPartialViewLocationFormats = new[] {
                 "~/Areas/{2}/Views/{1}/{0}.cshtml", 
-                "~/Areas/{2}/Views/{1}/{0}.vbhtml", 
-                "~/Areas/{2}/Views/Shared/{0}.cshtml", 
-                "~/Areas/{2}/Views/Shared/{0}.vbhtml"
+                "~/Areas/{2}/Views/Shared/{0}.cshtml"
             };
             base.ViewLocationFormats = new[] {
                 "~/Views/{1}/{0}.cshtml", 
-                "~/Views/{1}/{0}.vbhtml", 
-                "~/Views/Shared/{0}.cshtml", 
-                "~/Views/Shared/{0}.vbhtml"
+                "~/Views/Shared/{0}.cshtml"
             };
             base.MasterLocationFormats = new[] {
                 "~/Views/{1}/{0}.cshtml", 
-                "~/Views/{1}/{0}.vbhtml", 
-                "~/Views/Shared/{0}.cshtml", 
-                "~/Views/Shared/{0}.vbhtml"
+                "~/Views/Shared/{0}.cshtml"
             };
             base.PartialViewLocationFormats = new[] {
                 "~/Views/{1}/{0}.cshtml", 
-                "~/Views/{1}/{0}.vbhtml", 
-                "~/Views/Shared/{0}.cshtml", 
-                "~/Views/Shared/{0}.vbhtml"
+                "~/Views/Shared/{0}.cshtml"
             };
             base.FileExtensions = new[] {
-                "cshtml", 
-                "vbhtml"
+                "cshtml"
             };
         }
 
         protected override IView CreatePartialView(ControllerContext controllerContext, string partialPath) {
             Type type = GetTypeFromVirtualPath(partialPath);
-            if (type != null) {
-                return new RoslynRazorView(partialPath, type, false, base.FileExtensions);
-            }
-            return null;
+            return new RoslynRazorView(partialPath, type, false, base.FileExtensions);
         }
 
         protected override IView CreateView(ControllerContext controllerContext, string viewPath, string masterPath) {
             Type type = GetTypeFromVirtualPath(viewPath);
-            if (type != null) {
-                return new RoslynRazorView(viewPath, type, true, base.FileExtensions);
-            }
-            return null;
+            return new RoslynRazorView(viewPath, type, true, base.FileExtensions);
         }
 
         public object CreateInstance(string virtualPath) {
             Type type = GetTypeFromVirtualPath(virtualPath);
-
-            if (type != null) {
-                return Activator.CreateInstance(type);
-            }
-            return null;
+            return Activator.CreateInstance(type);
         }
 
         public bool Exists(string virtualPath) {
@@ -93,29 +72,63 @@ namespace RoslynRazorViewEngine {
         }
 
         private Type GetTypeFromVirtualPath(string virtualPath) {
+
             virtualPath = VirtualPathUtility.ToAbsolute(virtualPath);
 
-            WebPageRazorHost host = WebRazorHostFactory.CreateHostFromConfig(virtualPath);
+            string cacheKey = "RoslynRazor_" + virtualPath;
 
-            RazorTemplateEngine engine = new RazorTemplateEngine(host);
+            Type type = (Type)HttpRuntime.Cache[cacheKey];
+
+            if (type == null) {
+                DateTime utcStart = DateTime.UtcNow;
+
+                type = GetTypeFromVirtualPathNoCache(virtualPath);
+
+                // Cache it, and make it dependent on the razor file
+                CacheDependency cacheDependency = HostingEnvironment.VirtualPathProvider.GetCacheDependency(virtualPath, new string[] { virtualPath }, utcStart);
+                HttpRuntime.Cache.Insert(cacheKey, type, cacheDependency);
+            }
+
+            return type;
+        }
+
+        private Type GetTypeFromVirtualPathNoCache(string virtualPath) {
+            WebPageRazorHost host = WebRazorHostFactory.CreateHostFromConfig(virtualPath);
+            string code = GenerateCodeFromRazorTemplate(host, virtualPath);
+
+            var assembly = CompileCodeIntoAssembly(code, virtualPath);
+
+            return assembly.GetType(String.Format(CultureInfo.CurrentCulture, "{0}.{1}", host.DefaultNamespace, host.DefaultClassName));
+        }
+
+        private string GenerateCodeFromRazorTemplate(WebPageRazorHost host, string virtualPath) {
+            host = WebRazorHostFactory.CreateHostFromConfig(virtualPath);
+            VirtualPathProvider vpp = HostingEnvironment.VirtualPathProvider;
+            var engine = new RazorTemplateEngine(host);
             GeneratorResults results = null;
-            using (var stream = VirtualPathProvider.OpenFile(virtualPath)) {
+            VirtualFile file = vpp.GetFile(virtualPath);
+            using (var stream = file.Open()) {
                 using (TextReader reader = new StreamReader(stream)) {
                     results = engine.GenerateCode(reader, className: null, rootNamespace: null, sourceFileName: host.PhysicalPath);
                 }
             }
-            //if (!results.Success) {
-            //    throw CreateExceptionFromParserError(results.ParserErrors.Last(), VirtualPath);
-            //}
-            //_generatedCode = results.GeneratedCode;
+
+            if (!results.Success) {
+                throw CreateExceptionFromParserError(results.ParserErrors.Last(), virtualPath);
+            }
 
             var codeDomProvider = new CSharpCodeProvider();
             var srcFileWriter = new StringWriter();
             codeDomProvider.GenerateCodeFromCompileUnit(results.GeneratedCode, srcFileWriter, new CodeGeneratorOptions());
 
-            string code = srcFileWriter.ToString();
+            return srcFileWriter.ToString();
+        }
+
+        private Assembly CompileCodeIntoAssembly(string code, string virtualPath) {
+            // Parse the source file using Roslyn
             var syntaxTree = SyntaxTree.ParseCompilationUnit(code);
 
+            // Add all the references we need for the compilation
             var references = new List<AssemblyFileReference>();
             foreach (Assembly referencedAssembly in BuildManager.GetReferencedAssemblies()) {
                 references.Add(new AssemblyFileReference(referencedAssembly.Location));
@@ -123,21 +136,26 @@ namespace RoslynRazorViewEngine {
 
             var compilationOptions = new CompilationOptions(assemblyKind: AssemblyKind.DynamicallyLinkedLibrary);
 
-            var compilation = Compilation.Create("qqq", compilationOptions, new[] { syntaxTree }, references);
+            // Note: using a fixed assembly name, which doesn't matter as long as we don't expect cross references of generated assemblies
+            var compilation = Compilation.Create("SomeAssemblyName", compilationOptions, new[] { syntaxTree }, references);
 
+            // Generate the assembly into a memory stream
             var memStream = new MemoryStream();
-            var emitResult = compilation.Emit(memStream);
+            EmitResult emitResult = compilation.Emit(memStream);
 
             if (!emitResult.Success) {
-                foreach (Diagnostic diagnostic in emitResult.Diagnostics) {
-                    throw new Exception(diagnostic.Info.ToString());
-                }
-                return null;
+                Diagnostic diagnostic = emitResult.Diagnostics.First();
+                string message = diagnostic.Info.ToString();
+                LinePosition linePosition = diagnostic.Location.GetLineSpan(usePreprocessorDirectives: true).StartLinePosition;
+
+                throw new HttpParseException(message, null, virtualPath, null, linePosition.Line + 1);
             }
 
-            var assembly = Assembly.Load(memStream.GetBuffer());
+            return Assembly.Load(memStream.GetBuffer());
+        }
 
-            return assembly.GetType(String.Format(CultureInfo.CurrentCulture, "{0}.{1}", host.DefaultNamespace, host.DefaultClassName));
+        private HttpParseException CreateExceptionFromParserError(RazorError error, string virtualPath) {
+            return new HttpParseException(error.Message + Environment.NewLine, null, virtualPath, null, error.Location.LineIndex + 1);
         }
     }
 }
